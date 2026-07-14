416b1b205e2deb0487d41fbd85a553ff7561ce59
Sabarivasan S <s.sabarivasan2007@gmail.com>
2026-07-14 10:56:58 +0530

    Today i have learn about number crunching basics
    Today i have learn about number crunching basics with using while loop

---
+++ Day4/To_Do.java
@@ -0,0 +1,24 @@
+//package Day4;
+//import java.lang.classfile.attribute.RuntimeInvisibleAnnotationsAttribute;
+//import java.util.Scanner;
+//import java.util.Random;
+//public class To_Do {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        Random rand=new Random();
+//        int n=rand.nextInt(10);
+//        int guess;
+//        do {
+//            System.out.println("Enter the value of n : ");
+//            guess = sc.nextInt();
+//            if (guess > n) {
+//                System.out.println("The guess is too High");
+//            } else if (guess < n) {
+//                System.out.println("The guess is too Low");
+//            }
+//        }
+//            while(n!=guess);{
+//                System.out.println("Got it!");
+//            }
+//    }
+//}
---
+++ Day4/numcrunch.java
@@ -0,0 +1,24 @@
+//package Day4;
+//import java.util.Scanner;
+//public class numcrunch {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);{
+//            int num=sc.nextInt();
+//            int pop;
+//            int find=sc.nextInt();
+//            int count=0;
+//            while(num!=0){
+//                pop=num%10;
+//                num/=10;
+//                if(pop==find){
+//                    count+=1;
+//                }
+//              else{
+//                    System.out.println("My number doesn't contains ");
+//                   break;
+//              }
+//            }
+//            System.out.println("My number has the "+ find + " count of : "+ count);
+//        }
+//    }
+//}
