4148530cee4d10d1aa01f2310e1341bfac42949d
Sabarivasan S <s.sabarivasan2007@gmail.com>
2026-07-29 10:18:54 +0530

    Today i have learn about collections
    Today i have learn about collections and list using array list

---
+++ Day15/collection.java
@@ -0,0 +1,11 @@
+package Day15;
+import java.util.ArrayList;
+public class collection {
+    public static void main(String[] args) {
+        ArrayList <Object> name=new ArrayList<>();
+        name.add("Nithin");
+        name.add("5622");
+        name.add("U");
+        System.out.println(name);
+    }
+}
---
+++ Day15/list.java
@@ -0,0 +1,131 @@
+//package Day15;
+//import java.util.ArraysList;
+//import java.util.List;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Object> name = Arrays.asList(1, 2, 35, 88, "Sabari", 'i');
+//        name.set(0,"hello");
+//        System.out.println(name);
+//    }
+//}
+
+//printin the values in set list
+package Day15;
+import java.util.ArrayList;
+import java.util.List;
+public class list {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(1,20);
+//        list.set(1,2);
+        System.out.println(list);
+    }
+}
+
+//printing the values by merging two list:
+//package Day15;
+//import java.util.ArrayList;
+//import java.util.List;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>();
+//        list.add(2);
+//        list.add(1,20);
+//        list.set(1,2);
+//        List<Integer> list2 = new ArrayList<>();
+//        list2.add(3);
+//        list2.add(4);
+//        list2.add(5);
+//        list.addAll(list2);
+//        System.out.println(list);
+//    }
+//}
+
+//Removal methods in list (removing the elements present in the list):
+//package Day15;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer>list=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+//        List<Integer> list1=new ArrayList<>(Arrays.asList(6,7,8,9,10));
+//        list.remove(1);
+//        list.remove(3);
+//        System.out.println(list);
+//    }
+//}
+
+//Removal methods in list (removing all the elements present in the list):
+//package Day15;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+//    }
+//}
+
+//Removing the element by using remove-if condition to short list by condition
+//package Day15;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+//        list.removeIf(i -> i%2==0);
+//        System.out.println(list);
+//    }
+//}
+
+//Access & Search Methods:
+//package Day15;
+//import java.util.Arrays;
+//import  java.util.ArrayList;
+//import java.util.List;
+//import static javax.swing.UIManager.get;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer> list=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+//        System.out.println(list.get(2));
+//    }
+//}
+
+//printing LastIndexOf elements:
+//package Day15;
+//import java.util.Arrays;
+//import  java.util.ArrayList;
+//import java.util.List;
+//import static javax.swing.UIManager.get;
+//public class list {
+//    public static void main(String[] args) {
+//        List<Integer> list=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+//        System.out.println(list.indexOf(2));
+//    }
+//}
+
+//Printing lastIndexof String  (Pending):
+//package Day15;
+//import java.util.Arrays;
+//import  java.util.ArrayList;
+//import java.util.List;
+//import static javax.swing.UIManager.get;
+//public class list {
+//    public static void main(String[] args) {
+//        int LastIndex=list.lastIndexOf("Banana");
+//        System.out.println(list.indexOf('banana'));
+//    }
+//}
+
+//
+//package Day15;
+//import
+//public class list{
+//    public static void main(String args[]){
+//
+//    }
+//}
